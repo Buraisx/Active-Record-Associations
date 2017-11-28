ab950c6fc66c9bdfb40a355aad029215610365d6
Situ William <situ_william@yahoo.com>
2017-11-28 18:22:47 -0500

    added associations for part 4

--- chinook_associations/app/models/track.rb
+++ chinook_associations/app/models/track.rb
@@ -3,4 +3,6 @@ class Track < ApplicationRecord
   belongs_to :genre
   belongs_to :media_type
   has_and_belongs_to_many :playlists
+  # has_many :playlisttracks
+  # has_many :playlists, through: :playlisttracks
 end
